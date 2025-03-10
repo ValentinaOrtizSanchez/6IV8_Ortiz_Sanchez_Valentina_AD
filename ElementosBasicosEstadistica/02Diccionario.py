4abb99cc595a7eada0d8850ba2b5d65089c08de5
Valentina <ortiz.sanchez.valentina07@gmail.com>
2025-03-10 15:41:00 -0600

    Clase 10-03-25

--- ElementosBasicosEstadistica/02Diccionario.py
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 def estadistica_notas(notas):
     notas = pd.Series(notas)
-    estadisticas = pd.Series(notas.min(), notas.max(), notas.mean(), notas.std())
+    estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min', 'Max', 'Media', 'Desviación Estándar'])
+    return estadisticas
+
+notas = {'Juan': 9, 'Juanita': 7, 'Pedro': 6.6, 'Fabian': 8.5, 'Maximiliano': 7.5, 'Sandra': 9.8, 'Rosario': 9}
+
+print(estadistica_notas(notas))
     
